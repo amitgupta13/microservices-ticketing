ff8ba35a7b938c69559a4a9624559e16ab3259b8
abc <abc@abc.com>
2023-08-07 01:47:00 +0530

    added mongo deployment/service, and added mongo connection and model in auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
-
+import mongoose from "mongoose";
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signupRouter } from "./routes/signup";
@@ -21,4 +21,15 @@ app.all("*", () => {
 });
 app.use(errorHandler);
 
-app.listen(3000, () => console.log("Listening on port 3000"));
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Connected to mongodb");
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => console.log("Listening on port 3000"));
+};
+
+start();
